f8813cb58f05d31c568cffdefc07867cb79a4850
Constantine Kiriaze <ckiriaze@gmail.com>
2013-11-27 15:19:16 -0800

    init

---
+++ class-simple-plugin-name.php
@@ -0,0 +1,347 @@
+<?php
+/**
+* Simple Plugin Name.
+*
+* @package   Simple_Plugin_Name_Class
+* @author    Constantine Kiriaze, hello@kiriaze.com
+* @license   GPL-2.0+
+* @link      http://getsimple.io
+* @copyright 2013 Constantine Kiriaze
+*
+*
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists( 'Simple_Plugin_Name_Class' ) ) :
+
+class Simple_Plugin_Name_Class {
+
+    function __construct() {
+
+        //  Grab globals passed from init
+        global $cpt_slug, $cpt_name, $cpt_plural, $cpt_tax, $heirarchial, $has_archive, $rewrite, $defaultStyles;
+
+        //  Set them relative to function
+        $this->cpt_slug     = $cpt_slug;
+        $this->cpt_name     = $cpt_name;
+        $this->cpt_plural   = $cpt_plural;
+        $this->cpt_tax      = $cpt_tax;
+        $this->heirarchial  = $heirarchial;
+        $this->has_archive  = $has_archive;
+        $this->rewrite      = $rewrite;
+        $this->defaultStyles = $defaultStyles;
+    
+        //  Plugin Activation
+        register_activation_hook( __FILE__, array( &$this, 'plugin_activation' ) );
+        
+        //  Translation
+        load_plugin_textdomain( 'simple', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+        
+        //  Thumbnails
+        add_theme_support( 'post-thumbnails', array( $this->cpt_slug ) );
+        
+        //  CPT/Tax & Meta
+        add_action( 'init', array( &$this, 'cpt_init' ) );
+        add_action( 'init', array( &$this, 'add_cpt_meta' ) );
+
+        //  Columns
+        add_filter( 'manage_edit-'.$this->cpt_slug.'_columns', array( &$this, 'add_cpt_columns'), 10, 1 );
+        add_action( 'manage_'.$this->cpt_slug.'_posts_custom_column', array( &$this, 'display_cpt_columns' ), 10, 1 );
+        add_filter( 'manage_edit-'.$this->cpt_slug.'_sortable_columns', array( &$this, 'cpt_columns_register_sortable' ) );
+
+        add_action( 'right_now_content_table_end', array( &$this, 'add_cpt_counts' ) );
+        add_action( 'admin_head', array( &$this, 'cpt_icon' ) );
+
+        if( $this->defaultStyles ) {
+            //  Load template styles
+            add_action( 'wp_enqueue_scripts', array( &$this, 'load_styles' ), 101 );
+        }
+
+    }
+    
+    //  FLUSH REWRITE RULES
+    function plugin_activation() {
+        flush_rewrite_rules();
+    }
+    
+    function cpt_init() {
+
+        //  Register cpt / tax
+        $post_types = array(
+                
+            $this->cpt_slug =>  array(
+                'labels'                    => array(
+                    'name'                      => __( $this->cpt_plural ),
+                    'singular_name'             => __( $this->cpt_name ),
+                    'add_new'                   => __( 'Add New ' . $this->cpt_name ),
+                    'add_new_item'              => __( 'Add New ' . $this->cpt_name ),
+                    'edit_item'                 => __( 'Edit ' . $this->cpt_name ),
+                    'new_item'                  => __( 'Add New ' . $this->cpt_name ),
+                    'view_item'                 => __( 'View ' . $this->cpt_name ),
+                    'search_items'              => __( 'Search ' . $this->cpt_plural ),
+                    'not_found'                 => __( 'No '. $this->cpt_plural .' found' ),
+                    'not_found_in_trash'        => __( 'No '. $this->cpt_plural .' found in trash' )
+                ),
+                'public'                    => true,
+                'supports'                  => array( 'title', 'editor','thumbnail'),
+                'capability_type'           => 'post',
+                'menu_position'             => '15',
+                'hierarchical'              => $this->heirarchial,
+                'has_archive'               => $this->has_archive,
+                'rewrite'                   => unserialize($this->rewrite)
+            )
+        );
+
+        foreach( $post_types as $post_type => $args ) {
+            register_post_type( $post_type, $args );
+        };
+
+        global $taxonomies;
+
+        $taxonomies = array(
+            $this->cpt_slug . '_tag_labels'         => array(
+                'object_type'                   => $this->cpt_slug,
+                'label'                         => $this->cpt_name. ' Tags',
+                'labels'                        => array(
+                        'name'                      => $this->cpt_name. ' Tags',
+                        'singluar_name'             => substr_replace( $this->cpt_name. ' Tags', "", -1 ),
+                    ),
+                'public'                        => true,
+                'show_in_nav_menus'             => false,
+                'show_ui'                       => true,
+                'show_tagcloud'                 => false,
+                'hierarchical'                  => true,
+                'rewrite'                       => array('slug' => $this->cpt_slug . '_tag'),
+                'link_to_post_type'             => false,
+                'post_type_link'                => null,
+                'has_archive'                   => true  
+            ),
+            $this->cpt_slug . '_category_labels'    => array(
+                'object_type'                   => $this->cpt_slug,
+                'label'                         => $this->cpt_name. ' Categories',
+                'labels'                        => array(
+                        'name'                      => $this->cpt_name. ' Categories',
+                        'singluar_name'             => substr_replace( $this->cpt_name. ' Categories', "", -1 ),
+                    ),
+                'public'                        => true,
+                'show_in_nav_menus'             => false,
+                'show_ui'                       => true,
+                'show_tagcloud'                 => false,
+                'hierarchical'                  => true,
+                'rewrite'                       => array('slug' => $this->cpt_slug . '_category'),
+                'link_to_post_type'             => false,
+                'post_type_link'                => null,
+                'has_archive'                   => true  
+            ),
+
+        );
+
+        // conditional check if custom tax set
+        if( $this->cpt_tax ) :
+
+            $custom_tax = array(
+                preg_replace("/\W/", "_", strtolower($this->cpt_tax) )    => array(
+                    'object_type'                   => $this->cpt_slug,
+                    'label'                         => $this->cpt_tax,
+                    'labels'                        => array(
+                            'name'                      => $this->cpt_tax,
+                            'singluar_name'             => substr_replace( $this->cpt_tax.'s', "", -1 ),
+                        ),
+                    'public'                        => true,
+                    'show_in_nav_menus'             => false,
+                    'show_ui'                       => true,
+                    'show_tagcloud'                 => false,
+                    'hierarchical'                  => true,
+                    'rewrite'                       => array('slug' => preg_replace("/\W/", "-", strtolower($this->cpt_tax) ) ),
+                    'link_to_post_type'             => false,
+                    'post_type_link'                => null,
+                    'has_archive'                   => true  
+                )
+            );
+            
+            $taxonomies = array_merge($taxonomies, $custom_tax);
+
+        endif;
+
+        global $association_array;
+        $association_array = array();
+
+        foreach( $taxonomies as $taxonmy => $args ) {
+            
+            register_taxonomy( $taxonmy, $args['object_type'], $args );
+            
+            if( $args['link_to_post_type'] )
+            $association_array[$taxonmy] = $args['post_type_link'];
+            
+        }
+
+    }
+
+    //  Add CPT Meta
+    function add_cpt_meta() {
+
+        if(function_exists("register_field_group")) {
+            // Your custom acf meta
+            register_field_group(array (
+                'id' => 'acf_' . $this->cpt_slug,
+                'title' => $this->cpt_name,
+                'fields' => array (
+                    array (
+                        'key' => 'field_5254d4ad4csd4',
+                        'label' => $this->cpt_name . ' ID',
+                        'name' => $this->cpt_slug . '_id',
+                        'type' => 'text',
+                        'default_value' => '',
+                        'placeholder' => '',
+                        'prepend' => '',
+                        'append' => '',
+                        'formatting' => 'html',
+                        'maxlength' => '',
+                    ),
+                    array (
+                        'key' => 'field_5254d4b44csd5',
+                        'label' => $this->cpt_name . ' Value',
+                        'name' => $this->cpt_slug . '_value',
+                        'type' => 'select',
+                        'instructions' => 'Pick a value listed below for your '.$this->cpt_name.'!',
+                        'choices' => array (
+                            'value-1' => 'Value 1',
+                            'value-2' => 'Value 2',
+                            'value-3' => 'Value 3',
+                            'value-4' => 'Value 4',
+                            'value-5' => 'Value 5',
+                            'value-6' => 'Value 6',
+                            'value-7' => 'Value 7',
+                            'value-8' => 'Value 8',
+                        ),
+                        'default_value' => 'value-1',
+                        'allow_null' => 0,
+                        'multiple' => 0,
+                    ),
+                ),
+                'location' => array (
+                    array (
+                        array (
+                            'param' => 'post_type',
+                            'operator' => '==',
+                            'value' => $this->cpt_slug,
+                            'order_no' => 0,
+                            'group_no' => 0,
+                        ),
+                    ),
+                ),
+                'options' => array (
+                    'position' => 'side',
+                    'layout' => 'default',
+                    'hide_on_screen' => array (
+                        0 => 'custom_fields',
+                        1 => 'discussion',
+                        2 => 'comments',
+                        3 => 'revisions',
+                        4 => 'slug',
+                        5 => 'author',
+                        6 => 'format',
+                        7 => 'send-trackbacks',
+                    ),
+                ),
+                'menu_order' => 0,
+            ));
+        }
+    }
+
+    //  Add Columns
+    function add_cpt_columns( $columns ) {
+
+        $columns = array(
+            'cb'            => '<input type="checkbox" />',
+            'title'         => __( 'Name' ),
+            $this->cpt_slug . '_id'      => __( $this->cpt_name . ' ID' ),
+            $this->cpt_slug . '_value'  => __( $this->cpt_name . ' Value' ),
+            'date'          => __( 'Date' )
+        );
+
+        return $columns;
+    }
+    
+    //  Add data to columns
+    function display_cpt_columns( $column ) {
+        
+        global $post;
+        
+        switch ( $column ) {
+            
+            case $this->cpt_slug . '_id':
+            echo get_field($this->cpt_slug . '_id');
+                break;
+
+            case $this->cpt_slug . '_value':
+            $field = get_field_object($this->cpt_slug . '_value');
+            $value = get_field($this->cpt_slug . '_value');
+            $label = $field['choices'][ $value ];
+            echo $label;
+                break;
+
+
+            // Just break out of the switch statement for everything else.
+            default :
+                break;
+        }
+
+    }
+
+    //  Register the columns as sortable
+    function cpt_columns_register_sortable( $columns ) {
+        
+        $columns[$this->cpt_slug . '_id'] = $this->cpt_slug . '_id';
+        $columns[$this->cpt_slug . '_value'] = $this->cpt_slug . '_value';
+
+        return $columns;
+    }
+
+    //  Add count to dashboard widget
+    function add_cpt_counts() {
+
+        if ( ! post_type_exists( $this->cpt_slug ) ) {
+            return;
+        }
+
+        $num_posts = wp_count_posts( $this->cpt_slug );
+        $num = number_format_i18n( $num_posts->publish );
+        $text = _n( $this->cpt_name . '', $this->cpt_name . 's', intval($num_posts->publish) );
+        if ( current_user_can( 'edit_posts' ) ) {
+            $num = "<a href='edit.php?post_type=".$this->cpt_slug."'>$num</a>";
+            $text = "<a href='edit.php?post_type=".$this->cpt_slug."'>$text</a>";
+        }
+        echo '<td class="first b b-'.$this->cpt_slug.'">' . $num . '</td>';
+        echo '<td class="t '.$this->cpt_slug.'">' . $text . '</td>';
+        echo '</tr>';
+
+        if ($num_posts->pending > 0) {
+            $num = number_format_i18n( $num_posts->pending );
+            $text = _n( $this->cpt_name . ' Pending', $this->cpt_name . 's Pending', intval($num_posts->pending) );
+            if ( current_user_can( 'edit_posts' ) ) {
+                $num = "<a href='edit.php?post_status=pending&post_type=".$this->cpt_slug.">$num</a>";
+                $text = "<a href='edit.php?post_status=pending&post_type=".$this->cpt_slug.">$text</a>";
+            }
+            echo '<td class="first b b-'.$this->cpt_slug.'">' . $num . '</td>';
+            echo '<td class="t '.$this->cpt_slug.'">' . $text . '</td>';
+
+            echo '</tr>';
+        }
+    }
+
+    //  CUSTOM ICON FOR POST TYPE
+    function cpt_icon() {
+        // wp_enqueue_style( 'admin-plugin-name-css', plugins_url( 'assets/css/admin.css', __FILE__ ) );    
+    }
+
+    //  DEFAULT STYLES
+    function load_styles() {
+        // wp_enqueue_style( 'plugin-name-css', plugins_url( 'assets/css/plugin-name.css', __FILE__ ) );    
+    }
+}
+
+new Simple_Plugin_Name_Class;
+
+endif;
---
+++ simple-plugin-name.php
@@ -0,0 +1,121 @@
+<?php
+/**
+    *
+    * @package   Simple Plugin Name
+    * @author    Constantine Kiriaze, hello@kiriaze.com
+    * @license   GPL-2.0+
+    * @link      http://getsimple.io
+    * @copyright 2013 Constantine Kiriaze
+    *
+	* Plugin Name:     Simple Plugin Name
+	* Plugin URI:      http://getsimple.io
+	* Description:     Simple Plugin Name Description
+	* Version:         1.0
+	* Author:          Constantine Kiriaze (@kiriaze)
+	* Author URI:      http://getsimple.io/about
+    * Text Domain:     'simple' 
+    * Copyright:       (c) 2013, Constantine Kiriaze
+    * License:         GNU General Public License v2 or later
+    * License URI:     http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+/*
+    Copyright (C) 2013  Constantine Kiriaze (hello@kiriaze.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+
+
+// Example of setting constants within theme - Note: must be outside after_setup_theme()
+// define( 'SIMPLE_EXAMPLE_NAME', 'Events' );
+// define( 'SIMPLE_EXAMPLE_HIERARCHIAL', true );
+// define( 'SIMPLE_EXAMPLE_ARCHIVE', true );
+// define( 'SIMPLE_EXAMPLE_REWRITE_URL', 'concerts' );
+
+
+
+// Setup ACF
+// Check if plugin is activated, if not set up lite version included with simple or fallback to plugin acf
+if( ! class_exists('Acf') ) {
+    define( 'ACF_LITE' , true );
+    if ( file_exists( get_template_directory() . '/functions/advanced-custom-fields/acf.php' ) ) {
+        include_once( get_template_directory() . '/functions/advanced-custom-fields/acf.php' );
+    } else {
+        include_once( plugin_dir_path(__DIR__) . '/advanced-custom-fields/acf.php' );
+    }
+}
+
+//  Wrapped in after_setup_theme to utilize options
+add_action('after_setup_theme', 'simple_plugin_name_init');
+function simple_plugin_name_init(){
+
+    //  Define Globals
+    $plugin_name        =   'Simple Plugin Name';   // change this - always prefix e.g. Simple Staff
+    $cpt_name           =   'Custom Post Type';     // change this to post type singular - e.g. Event
+    $plugin_name        =   preg_replace("/\W/", "-", strtolower($plugin_name) );
+    $prefix             =   preg_replace("/\W/", "_", strtolower($plugin_name) );
+    $plugin_url         =   plugin_dir_url( __FILE__ );
+    $plugin_path        =   plugin_dir_path( __FILE__ );
+    $plugin_basename    =   plugin_basename( __FILE__ );
+
+    //  Grab all options
+    if ( function_exists( 'of_get_option' ) ) {
+    
+        $cpt_option         = of_get_option( $prefix.'_name' );
+        $tax_option         = of_get_option( $prefix.'_tax' );
+        $heirarchial_option = of_get_option( $prefix.'_options' )['heirarchial'];
+        $archive_option     = of_get_option( $prefix.'_options' )['has_archive'];
+        $rewriteCheckbox    = of_get_option( $prefix.'_options' )['rewrite'];
+        $rewriteUrl         = !empty(of_get_option( $prefix . '_options_rewrite' )) && $rewriteCheckbox != 0 ? of_get_option( $prefix . '_options_rewrite' ) : '';
+
+        $defaultStyles      = of_get_option( $prefix.'_default_styles' );
+
+    }
+
+    $cpt_name           = !empty($cpt_option) ? $cpt_option : $cpt_name;
+    $cpt_tax            = !empty($tax_option) ? $tax_option : null;
+
+    //  Set globals if constants not defined    
+    $cpt_name           = defined( strtoupper($prefix).'_NAME' ) ? constant( strtoupper($prefix) . '_NAME' ) : $cpt_name;
+    $cpt_slug           = 'simple-' . preg_replace("/\W/", "-", strtolower($cpt_name) );
+    $cpt_plural         = $cpt_name .'s';
+    $cpt_tax            = defined( strtoupper($prefix).'_TAX' ) ? constant( strtoupper($prefix) . '_TAX' ) : $cpt_tax;
+
+
+    $heirarchial        = !empty($heirarchial_option) ? $heirarchial_option : false;
+    $heirarchial        = defined( strtoupper($prefix).'_HIERARCHIAL' ) ? constant( strtoupper($prefix) . '_HIERARCHIAL' ) : $heirarchial;
+
+    $has_archive        = !empty($archive_option) ? $archive_option : false;
+    $has_archive        = defined( strtoupper($prefix).'_ARCHIVE' ) ? constant( strtoupper($prefix) . '_ARCHIVE' ) : $has_archive;
+
+    $rewriteUrl         = defined( strtoupper($prefix).'_REWRITE_URL' ) ? constant( strtoupper($prefix) . '_REWRITE_URL' ) : $rewriteUrl;
+
+
+    //  Rewrite checking values and serializing rewrite array
+    $fields             = array( 'slug' );
+    $str                = "$rewriteUrl";
+    $rewrite            = ( $rewriteUrl != 'false' ) ? serialize(array_combine( $fields, explode ( ", ", $str ) )) : 'false';
+    
+
+    global $prefix, $cpt_slug, $cpt_name, $cpt_plural, $cpt_tax, $heirarchial, $has_archive, $rewrite, $defaultStyles;
+
+    //  Load class
+    require_once( $plugin_path . 'class-'.$plugin_name.'.php' );
+}
